f213f31826b6906a37fe45516f2b25f64ab13cac
John Elder <johne4196@gmail.com>
2020-10-29 11:36:23 -0700

    input boxes and buttons

--- input.py
+++ input.py
@@ -1,9 +1,9 @@
 import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+#from kivy.app import App
+#from kivy.uix.label import Label
+#from kivy.uix.gridlayout import GridLayout
+#from kivy.uix.textinput import TextInput
+#from kivy.uix.button import Button
 
 class MyGridLayout(GridLayout):
 	# Initialize infinite keywords
