e8b614451907da37f91ea132407fcd3f9fe88dc5
John Elder <johne4196@gmail.com>
2020-11-06 07:55:47 -0800

    kivy builder

--- builder.py
+++ builder.py
@@ -1,11 +1,50 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
+Builder.load_file('whatever.kv')
+'''Builder.load_string(""" 
+
+<MyGridLayout>
+	
+	name:name
+	pizza:pizza
+	color:color
+
+	GridLayout:
+		cols:1
+		size: root.width, root.height
+		GridLayout:
+			cols:2
+
+			Label:
+				text: "Name"
+			TextInput:
+				id: name
+				multiline:False
+
+			Label:
+				text: "Favorite Pizza"
+			TextInput:
+				id: pizza
+				multiline:False
+				
+			Label:
+				text: "Favorite Color"
+			TextInput:
+				id: color
+				multiline:False	
+
+		Button:
+			text: "Submit"
+			font_size: 32
+			on_press: root.press()
+
+
+
+	""")
+'''
 class MyGridLayout(Widget):
 	
 	name = ObjectProperty(None)
@@ -27,13 +66,13 @@ class MyGridLayout(Widget):
 		self.pizza.text = ""
 		self.color.text = ""
 
-class MyApp(App):
+class AwesomeApp(App):
 	def build(self):
 		return MyGridLayout()
 
 
 if __name__ == '__main__':
-	MyApp().run()
+	AwesomeApp().run()
 
 
 
