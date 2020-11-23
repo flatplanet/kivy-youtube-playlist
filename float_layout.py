45a7cbb8d39c3e9aaedaa613f4b97a17b1396044
John Elder <johne4196@gmail.com>
2020-11-23 08:56:27 -0800

    floatlayout

---
+++ float_layout.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+#from kivy.uix.floatlayout import FloatLayout
+
+# Designate Our .kv design file 
+Builder.load_file('float_layout.kv')
+
+class MyLayout(Widget):
+	pass
+
+class AwesomeApp(App):
+	def build(self):
+		Window.clearcolor = (1,1,1,1)
+		return MyLayout()
+
+if __name__ == '__main__':
+	AwesomeApp().run()
+
+
+
