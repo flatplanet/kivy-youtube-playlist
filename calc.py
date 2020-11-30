a9f0c4c72d8b5418c36bdcd260af81fad024dbba
John Elder <johne4196@gmail.com>
2020-11-30 08:51:11 -0800

    addition

--- calc.py
+++ calc.py
@@ -14,6 +14,65 @@ class MyLayout(Widget):
 	def clear(self):
 		self.ids.calc_input.text = '0'
 
+	# Create a button pressing function
+	def button_press(self, button):
+		# create a variable that contains whatever was in the text box already
+		prior = self.ids.calc_input.text
+		
+		# determine if 0 is sitting there
+		if prior == "0":
+				self.ids.calc_input.text = ''
+				self.ids.calc_input.text = f'{button}'
+		else: 
+			self.ids.calc_input.text = f'{prior}{button}'
+
+	# create addition function
+	def add(self):
+		# create a variable that contains whatever was in the text box already
+		prior = self.ids.calc_input.text
+		
+		# slap a plus sign to the text box
+		self.ids.calc_input.text = f'{prior}+'
+
+	# create addition function
+	def subtract(self):
+		# create a variable that contains whatever was in the text box already
+		prior = self.ids.calc_input.text
+		
+		# slap a plus sign to the text box
+		self.ids.calc_input.text = f'{prior}-'
+
+	# create addition function
+	def multiply(self):
+		# create a variable that contains whatever was in the text box already
+		prior = self.ids.calc_input.text
+		
+		# slap a plus sign to the text box
+		self.ids.calc_input.text = f'{prior}*'
+
+	# create addition function
+	def divide(self):
+		# create a variable that contains whatever was in the text box already
+		prior = self.ids.calc_input.text
+		
+		# slap a plus sign to the text box
+		self.ids.calc_input.text = f'{prior}/'
+
+	# create equals to function
+	def equals(self):
+		prior = self.ids.calc_input.text
+
+		# Addition
+		if "+" in prior:
+			num_list = prior.split("+")
+			answer = 0
+			# loop thru our list
+			for number in num_list:
+				answer = answer + int(number)
+
+			# print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+
 
 class CalculatorApp(App):
 	def build(self):
